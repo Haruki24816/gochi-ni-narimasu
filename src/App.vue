0600a0988d92a54bc1c42ec34b9b70cdb491f552
Haruki <haruki24816@gmail.com>
2022-12-26 01:34:49 +0900

    ちょっと構成を変更

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
-  <Navbar/>
-  <Board/>
+  <Navbar>
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ToggleAssist />
+      <ChangeSound />
+      <Reset />
+      <li class="nav-item"><a class="nav-link" href="/sheet.pdf">記録シートDL</a></li>
+    </ul>
+    <form class="d-flex">
+      <input class="form-control me-2" type="text" placeholder="プレイヤー名" aria-label="プレイヤー名">
+      <button class="btn btn-outline-success flex-shrink-0">追加</button>
+    </form>
+  </Navbar>
+  <Board />
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue"
 import Board from "./components/Board.vue"
+import ToggleAssist from "./components/ToggleAssist.vue"
+import ChangeSound from "./components/ChangeSound.vue"
+import Reset from "./components/Reset.vue"
 
 export default {
   name: "App",
   components: {
     Navbar,
-    Board
+    Board,
+    ToggleAssist,
+    ChangeSound,
+    Reset
   }
 }
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,32 +9,14 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <ToggleAssist/>
-          <ChangeSound/>
-          <Reset/>
-          <li class="nav-item"><a class="nav-link" href="/sheet.pdf">記録シートDL</a></li>
-        </ul>
-        <form class="d-flex">
-          <input class="form-control me-2" type="text" placeholder="プレイヤー名" aria-label="プレイヤー名">
-          <button class="btn btn-outline-success flex-shrink-0">追加</button>
-        </form>
+        <slot></slot>
       </div>
     </div>
   </nav>
 </template>
 
 <script>
-import ToggleAssist from "./ToggleAssist.vue"
-import ChangeSound from "./ChangeSound.vue"
-import Reset from "./Reset.vue"
-
 export default {
-  name: "Navbar",
-  components: {
-    ToggleAssist,
-    ChangeSound,
-    Reset
-  }
+  name: "Navbar"
 }
 </script>
