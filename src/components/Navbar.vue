a5eca7b4ff5ed70a38fb27522927bb23f58adaeb
Haruki <haruki24816@gmail.com>
2022-12-26 00:55:45 +0900

    ナビゲーションバーの項目を追加

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,9 +11,32 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">プレイ補助 <span class="badge bg-primary" v-if="true">オン</span><span class="badge bg-secondary" v-else>オフ</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              サウンド変更
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item active" href="#">借金①</a></li>
+              <li><a class="dropdown-item" href="#">借金②</a></li>
+              <li><a class="dropdown-item" href="#">借金③</a></li>
+              <li><a class="dropdown-item" href="#">大破産</a></li>
+              <li><a class="dropdown-item" href="#">レジスター</a></li>
+              <li><a class="dropdown-item" href="#">爆発</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">リセット</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">記録シートDL</a>
           </li>
         </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="text" placeholder="プレイヤー名" aria-label="プレイヤー名">
+          <button class="btn btn-outline-success flex-shrink-0">追加</button>
+        </form>
       </div>
     </div>
   </nav>
