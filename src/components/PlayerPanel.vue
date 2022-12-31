4359fdb61563573cbaacb857227a6ff0ae0923cf
Haruki <haruki24816@gmail.com>
2022-12-31 23:37:38 +0900

    まあ一旦これで

--- src/components/PlayerPanel.vue
+++ src/components/PlayerPanel.vue
@@ -1,24 +1,28 @@
 <template>
-  <div class="alert alert-primary mt-3" role="alert">
-    <h4>{{ playerName }}</h4>
-    <ul class="mb-0">
-      <li v-for="(amount, opponentPlayer) in dataContent">
-        {{ opponentPlayer }}: {{ (amount).toLocaleString("ja-JP") }}円
-        <br>
-        <button type="button" class="btn btn-primary" @click="$emit('increase-debt', [playerName,opponentPlayer])">＋</button>
-        <button type="button" class="btn btn-primary" @click="$emit('decrease-debt', [playerName,opponentPlayer])">－</button>
-      </li>
-    </ul>
+  <h4>{{ playerName }}</h4>
+  <div class="row">
+    <div class="col" v-for="(amount, opponentPlayer) in dataContent">
+      <div>
+        {{ opponentPlayer }}<br>
+        {{ (amount).toLocaleString("ja-JP") }} 円
+      </div>
+      <div class="btn-group btn-group-lg" role="group" aria-label="input">
+        <button type="button" class="btn btn-primary"
+          @click="$emit('increase-debt', [playerName, opponentPlayer])">＋</button>
+        <button type="button" class="btn btn-primary"
+          @click="$emit('decrease-debt', [playerName, opponentPlayer])">－</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "PlayerPanel",
-    props: {
-      playerName: String,
-      dataContent: Object
-    }
+export default {
+  name: "PlayerPanel",
+  props: {
+    playerName: String,
+    dataContent: Object
   }
+}
 
 </script>
